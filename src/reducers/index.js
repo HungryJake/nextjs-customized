758f16109ad587e0a62dd8b8d751a2d85b7b420a
Jake Lu <BananaCloud2@gmail.com>
2021-04-13 11:17:18 -0700

    removed redux form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
-import { combineReducers } from 'redux'
-import { reducer as formReducer } from 'redux-form'
+import { combineReducers } from 'redux';
 import counterReducer from './counter.reducer';
 import pingReducer from './ping-api.reducer';
 
 export default combineReducers({
-  form: formReducer,
   counter: counterReducer,
   ping: pingReducer
-})
+});
